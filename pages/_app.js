811cadd07cb5571ba6e78e7eeced648f35837a28
Anthony Taveras <urawesomeman@hotmail.com>
2023-01-29 19:38:28 -0500

    breaking blog again (#4)

--- pages/_app.js
+++ pages/_app.js
@@ -2,4 +2,5 @@ import "./styles/global_styles.scss"
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+  </Applications/AMPPS/www/Vegan Food Blog>
 }
