f8a338e23907a59082e5af463db3d6f1e333d84c
Anthony Taveras <command76@gmail.com>
2023-01-29 20:14:16 -0500

    breaking it yet again

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "./styles/global_styles.scss"
 
 export default function MyApp({ Component, pageProps }) {
+  <iweiwiewieiwieewi></iweiwiewieiwieewi>
   return <Component {...pageProps} />
 }
