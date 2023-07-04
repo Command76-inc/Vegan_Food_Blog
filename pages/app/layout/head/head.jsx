04c131320f3f05ec2b77a8f20d51aeec1edfa711
Anthony Taveras <command76@gmail.com>
2023-07-04 19:56:38 -0400

    fixed heading

--- pages/app/layout/head/head.jsx
+++ pages/app/layout/head/head.jsx
@@ -1,41 +1,36 @@
-import Head from 'next/head'
-
-function Title({ title }) {
-  return <title>{title ? title: "Vegan App Home Page"}</title>
-}
-
-function OGTitle({ title }) {
-  return <meta property="og:title" content={title ? title: "Vegan App Home Page"} key="title" />
-}
-
-function Description({ description }) {
-  return <meta name="description" content={description ? description: "A place for all your vegan essentials"} key="description"></meta>
-}
-
-function OGDescription({ description}) {
-  return <meta name="og:description" content={description ? description: "A place for all your vegan essentials"} key="description"></meta>
-}
-
-function CanonicalLink({ page }) {
-  return <link rel="canonical" href={page ? page: "/"} ></link>
-}
-
-function Robots() {
-  return <meta name="robots" content="noindex/nofollow"></meta>
-}
+import Head from "next/head";
 
 export function ReplaceHead(props) {
-    return (
+  return (
     <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous"></link>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css"></link>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <meta http-equiv="content-type" content="text-html; charset=utf-8"></meta>
-        <Title title={props.title}></Title>
-        <OGTitle title={props.title}></OGTitle>
-        <Description description={props.description}></Description>
-        <OGDescription description={props.description}></OGDescription>
-        {props.canonical  === true ? <CanonicalLink>props.page</CanonicalLink>:<Robots></Robots>} 
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css"
+        integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls"
+        crossorigin="anonymous"
+      ></link>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css"
+      ></link>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
+      <meta http-equiv="content-type" content="text-html; charset=utf-8"></meta>
+      <title>{props.title}</title>
+      <meta property="og:title" content={props.title} key="title" />
+      <meta name="description" content={props.description} key="description" />
+      <meta
+        name="og:description"
+        content={props.description}
+        key="description"
+      />
+      {props.canonical === true ? (
+        <link rel="canonical" href={props.url} />
+      ) : (
+        <meta name="robots" content="noindex/nofollow" />
+      )}
     </Head>
-    )
+  );
 }
--- pages/components/layout.jsx
+++ pages/components/layout.jsx
@@ -3,18 +3,57 @@ import { Header } from "../app/layout/header/header";
 import { Wrapper } from "../app/layout/wrapper";
 import { Footer } from "../app/layout/footer/footer";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 export default function CreatePostLayout({
   children, // will be a page or nested layout
 }) {
   const router = useRouter();
+  const [baseUrl, setBaseUrl] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setBaseUrl(window.location.host);
+    }
+  });
+  const description = (route) => {
+    if (route.includes("posts")) {
+      return "The Vegan Blog | Posts";
+    } else if (route.includes("reviews")) {
+      return "The Vegan Blog | Reviews";
+    } else if (route.includes("recipes")) {
+      return "The Vegan Blog | Recipes";
+    } else if (route.includes("local_dining_areas")) {
+      return "The Vegan Blog | Local Dining Areas";
+    } else if (route.includes("about_us")) {
+      return "The Vegan Blog | About Us";
+    } else {
+      return "The Vegan Blog | Home";
+    }
+  };
+  const title = (route) => {
+    if (route.includes("posts")) {
+      return "The Vegan Blog | Posts";
+    } else if (route.includes("reviews")) {
+      return "The Vegan Blog | Reviews";
+    } else if (route.includes("recipes")) {
+      return "The Vegan Blog | Recipes";
+    } else if (route.includes("local_dining_areas")) {
+      return "The Vegan Blog | Local Dining Areas";
+    } else if (route.includes("about_us")) {
+      return "The Vegan Blog | About Us";
+    } else {
+      return "The Vegan Blog | Home";
+    }
+  };
 
   return (
     <section>
       <ReplaceHead
         canonical={true}
-        description={"blsdsd"}
-        title={"sdsdsd"}
+        description={description(router.route)}
+        title={title(router.route)}
+        url={baseUrl + router.route}
       ></ReplaceHead>
       <Header showBanner={router.route === "/" ? true : false} />
       {/* Include shared UI here e.g. a header or sidebar */}
