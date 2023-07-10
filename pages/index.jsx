e263ca4819aad8cdfd386ebc6022b89019241cbf
Reuben Taveras <rtaveras824@hotmail.com>
2023-07-10 07:46:49 -0400

    Updated banner and short intro section

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,8 @@ import styles from "./index.module.scss";
 import HomePagePosts from "./app/home_page/fetchPosts";
 import HomePageRecipes from "./app/home_page/fetchRecipes";
 import Image from "next/Image";
-import FLOWER_1_SVG from "./public/assets/Flower1.svg";
+import FLOWER_1_SVG from "./public/assets/Asset1.svg";
+import BRANCH_1_SVG from "./public/assets/Branch1.svg";
 
 const title = "Home | The Vegan Blog";
 const pageTitle = "The Vegan Blog";
@@ -17,18 +18,18 @@ export default function HomePage() {
   return (
     <div className={styles["home-container"]}>
       <div className={styles.intro}>
-        {/* <div className={styles["svg-wrapper"]}>
-          <Image className={styles["flower-1"]} src={FLOWER_1_SVG} alt="Flower" />
+        <div className={styles["svg-wrapper"]}>
+          <Image className={styles["flower-1"]} src={BRANCH_1_SVG} alt="Flower" />
         </div>
         <div className={styles["svg-wrapper"]}>
-          <Image className={styles["flower-1"]} src={FLOWER_1_SVG} alt="Flower" />
+          <Image className={styles["flower-1"]} src={BRANCH_1_SVG} alt="Flower" />
         </div>
         <div className={styles["svg-wrapper"]}>
-          <Image className={styles["flower-1"]} src={FLOWER_1_SVG} alt="Flower" />
+          <Image className={styles["flower-1"]} src={BRANCH_1_SVG} alt="Flower" />
         </div>
         <div className={styles["svg-wrapper"]}>
-          <Image className={styles["flower-1"]} src={FLOWER_1_SVG} alt="Flower" />
-        </div> */}
+          <Image className={styles["flower-1"]} src={BRANCH_1_SVG} alt="Flower" />
+        </div>
         <p>We'd like to walk along with you on your journey to a cruelty free and
           plant inspired lifestyle. You want to know we to eat, we can tell you.</p>
       </div>
