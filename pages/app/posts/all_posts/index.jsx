b05851723ccbfc85d63fcb4cad2d25f3340350e1
Anthony Taveras <urawesomeman@hotmail.com>
2023-07-20 10:33:57 -0400

    Removing some unused code (#25)
    * removing some no longer used code
    
    * deleting commented out code that is no longer needed

--- pages/app/posts/all_posts/index.jsx
+++ pages/app/posts/all_posts/index.jsx
@@ -2,10 +2,7 @@ import Link from "next/link";
 import styles from "../posts.module.scss";
 import { Breadcrumbs, Typography } from "@mui/material";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { ReplaceHead } from "../../layout/head/head";
-import { Header } from "../../layout/header/header";
 import { Wrapper } from "../../layout/wrapper";
-import { Footer } from "../../layout/footer/footer";
 import FetchPosts from "./fetchPosts";
 
 const title = "Blog | The Vegan Blog";
