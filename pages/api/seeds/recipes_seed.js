f908874cac8ab6562f8a93942c13070de595f02c
Anthony Taveras <command76@gmail.com>
2023-08-02 04:12:16 -0400

    got seed to populate recipes collection in local database by using postman api

--- pages/api/seeds/recipes_seed.js
+++ pages/api/seeds/recipes_seed.js
@@ -9,7 +9,7 @@ function getTitle() {
     "I can't believe this is real!",
   ];
 
-  return titles[Math.floor(Math.random() * 5)];
+  return titles[Math.floor(Math.random() * 4)];
 }
 
 function getDescription() {
@@ -36,7 +36,7 @@ function getDescription() {
   const sentence = new Array(10);
 
   for (let i = 0; i < sentence.length; i++) {
-    sentence[i] = wordsToUse[Math.floor(Math.random() * 17)];
+    sentence[i] = wordsToUse[Math.floor(Math.random() * 16)];
   }
 
   return sentence.join(" ");
@@ -45,13 +45,13 @@ function getDescription() {
 function getIngredients() {
   const wordsToUse = ["broccoli", "carrots", "spinach", "rice", "potatoes"];
 
-  const ingredients = new Array(3);
+  const ingredients = new Array(Math.ceil(Math.random() * 4));
 
   for (let i = 0; i < ingredients.length; i++) {
-    ingredients[i] = wordsToUse[Math.floor(Math.random() * 5)];
+    ingredients[i] = wordsToUse[Math.floor(Math.random() * 4)];
   }
 
-  return ingredients;
+  return { ingredients: ingredients, length: ingredients.length };
 }
 
 function getPrep() {
@@ -63,10 +63,10 @@ function getPrep() {
     "Pour oil and vinegar on spinach",
   ];
 
-  const prep = new Array(3);
+  const prep = new Array(Math.ceil(Math.random() * 4));
 
   for (let i = 0; i < prep.length; i++) {
-    prep[i] = wordsToUse[Math.floor(Math.random() * 5)];
+    prep[i] = wordsToUse[Math.floor(Math.random() * 4)];
   }
 
   return prep;
@@ -81,41 +81,51 @@ function getDirections() {
     "Steam cook carrots for 15 mins",
   ];
 
-  const directions = new Array(3);
+  const directions = new Array(Math.ceil(Math.random() * 4));
 
   for (let i = 0; i < directions.length; i++) {
-    directions[i] = wordsToUse[Math.floor(Math.random() * 5)];
+    directions[i] = wordsToUse[Math.floor(Math.random() * 4)];
   }
 
   return directions;
 }
 
 function getPrepTime() {
-  return Math.ceil(Math.random() * 120);
+  const time = Math.ceil(Math.random() * 120);
+  const hours = Math.floor(time / 60);
+  const minutes = time % 60;
+  return `${hours} ${hours !== 1 ? "hours" : "hour"} and ${minutes} ${
+    minutes !== 1 ? "minutes" : "minute"
+  }`;
 }
 
 function getCookTime() {
-  return Math.ceil(Math.random() * 120);
+  const time = Math.ceil(Math.random() * 120);
+  const hours = Math.floor(time / 60);
+  const minutes = time % 60;
+  return `${hours} ${hours !== 1 ? "hours" : "hour"} and ${minutes} ${
+    minutes !== 1 ? "minutes" : "minute"
+  }`;
 }
 
-function getIngredientsTotal() {
-  return getIngredients().length + 1;
-}
-
-export default async function insertSeedRecipeData() {
-  recipe = await db.Recipes;
+export default async function insertSeedRecipeData(req, res) {
+  const recipe = await db.Recipes;
   const documents = new Array(15);
   for (let i = 0; i < documents.length; i++) {
+    const { ingredients: ingredients, length: length } = getIngredients();
+
     documents[i] = {
       title: getTitle(),
       description: getDescription(),
-      ingredient: getIngredients(),
+      ingredient: ingredients,
       prep: getPrep(),
+      // in minutes
       prep_time: getPrepTime(),
       directions: getDirections(),
+      // in minutes
       cook_time: getCookTime(),
       // A total count of the ingredient array
-      ingredients: getIngredientsTotal(),
+      ingredients: length,
       // Points to an Amazon S3 bucket item URL
       dish_image: "https://aws.s3.bucket.url",
     };
@@ -123,5 +133,15 @@ export default async function insertSeedRecipeData() {
 
   recipe.insertMany(documents);
 
-  console.log(recipe);
+  if (req.method === "POST") {
+    if (res.status !== 200) {
+      res.send(
+        "Something went wrong, check terminal for any potential errors being logged."
+      );
+    } else {
+      res.status(200).send("Seed data should be inserted into local database");
+    }
+  } else {
+    res.status(400).send("Improper http method used");
+  }
 }
