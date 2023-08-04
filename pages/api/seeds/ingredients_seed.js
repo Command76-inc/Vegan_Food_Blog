84495195343c8e4dc8c4e195c3b1aa12cd6d4226
Anthony Taveras <command76@gmail.com>
2023-08-04 06:01:51 -0400

    added function definitions for creating seed data

--- pages/api/seeds/ingredients_seed.js
+++ pages/api/seeds/ingredients_seed.js
@@ -1,1 +1,81 @@
 import { db } from "../../../lib/db";
+
+function getTitle() {
+
+}
+
+function getServingSize() {
+
+}
+
+function getTotalFat() {
+
+}
+
+function getSaturatedFat() {
+
+}
+
+function getPolysaturatedFat() {
+
+}
+
+function getMonosaturatedFat() {
+
+}
+
+function getTransFat() {
+
+}
+
+function getCholesterol() {
+
+}
+
+function getSodium() {
+
+}
+
+function getTotalCarbohydrates() {
+
+}
+
+function getFiber() {
+
+}
+
+function getSugars() {
+
+}
+
+function getProtein() {
+
+}
+
+function getVitaminD() {
+
+}
+
+function getCalcium() {
+
+}
+
+function getIron() {
+
+}
+
+function getPotassium() {
+
+}
+
+function getCaffeine() {
+
+}
+
+function getDisclaimer() {
+
+}
+
+export default function insertIngredientSeedData(req, res) {
+
+}
