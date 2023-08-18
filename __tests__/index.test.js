b838c764a414663a388f49019ed86193ef7a1f8c
Anthony Taveras <anthonytaveras@Anthonys-MacBook-Pro-2.local>
2023-08-18 09:27:31 -0400

    created a passing test in the homepage, added prettier configuration to vscode

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,19 @@
 // Documentation for jest testing https://testing-library.com/docs/queries/byrole/
 // https://jestjs.io/docs/jest-platform
+// Documentation for react-testing-library https://testing-library.com/docs/dom-testing-library/cheatsheet/#queries
 // To run tests enter npm run test in terminal
 
+import next from "next";
+import { render, screen } from "@testing-library/react";
+import HomePage from "../pages/index";
+import "@testing-library/jest-dom";
 
-import { render, screen } from '@testing-library/react'
-import HomePage from '../pages/index'
-import '@testing-library/jest-dom'
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<HomePage />);
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<HomePage />)
- 
-    const heading = screen.getByRole('paragraph', {
-      name: /We'd like to walk along with /i,
-    })
- 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    const heading = screen.getByText(/We'd like to walk along with /i);
+
+    expect(heading).toBeInTheDocument();
+  });
+});
