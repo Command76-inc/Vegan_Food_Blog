e3c430c9d4cebb2c048e4e41c1b6a583687b463b
Anthony <anthony@Anthonys-MacBook-Pro-2.local>
2023-08-29 14:26:47 -0400

    added response object to get all recipes api

--- pages/api/recipes/get_recipes.js
+++ pages/api/recipes/get_recipes.js
@@ -1,6 +1,6 @@
 import { db } from "../../../lib/db";
 
-export default async function getAllRecipes() {
+export default async function getAllRecipes(req, res) {
   const recipes = await db.Recipes;
 
   const sortedRecipes = await recipes.find().sort({ updatedAt: "desc" });
