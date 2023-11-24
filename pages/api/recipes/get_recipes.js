11e7bf2510d271dc823f2bf112d7c110e6b1b0ab
Anthony Taveras <urawesomeman@hotmail.com>
2023-11-24 09:36:23 -0500

    Foodblog 18 early stage of recipe api int (#53)
    * added reviews, comments, users model to the db, only missing model is local dining areas
    
    * added email as field to user model
    
    * added request to api for the recipes page
    
    * Some minor non breaking changes with comments
    
    * fixed http headers issue, got data to populate on recipes page
    
    * removed needless comment
    
    ---------
    
    Co-authored-by: Anthony <anthony@Anthonys-MacBook-Pro-2.local>

--- pages/api/recipes/get_recipes.js
+++ pages/api/recipes/get_recipes.js
@@ -3,7 +3,7 @@ import { db } from "../../../lib/db";
 export default async function getAllRecipes(req, res) {
   const recipes = await db.Recipes;
 
-  const sortedRecipes = await recipes.find().sort({ updatedAt: "desc" });
+  const sortedRecipes = await recipes.find({}, null, {$sort: {"updatedAt": "desc"}});
 
   res.json(sortedRecipes);
 }
