1dc4ea5bbe8beed0c7beec53f50de8737674b78b
Anthony Taveras <urawesomeman@hotmail.com>
2024-07-19 22:05:55 -0400

    FOOD_BLOG_RETURN_1 - Adding HTTP request to single recipes page (#56)
    * refreshing on git again
    
    * added http request to single recipes page
    
    ---------
    
    Co-authored-by: Your Name <anthony@Anthonys-MacBook-Pro-2.local>

--- pages/app/recipes/single_recipe/index.jsx
+++ pages/app/recipes/single_recipe/index.jsx
@@ -3,8 +3,30 @@ import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { Wrapper } from "../../layout/wrapper";
 import styles from "../recipes.module.scss";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function SingleRecipe(props) {
+  const params = typeof window !== "undefined" ? window.location.search: Error;
+  const recipe = new URLSearchParams(params).getAll("id")[0]
+  const [data, setData] = useState("Recipe")
+  useEffect(() => {
+   (async () => {
+    const url = `/api/recipes/get_single_recipe?id=${recipe}`;
+    console.log(url)
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      setData(json)
+    } catch (error) {
+      console.error(error.message);
+    }
+  })()
+  }, [])
   return (
     <Wrapper className={props.className}>
       <main className={styles["recipes-container"]}>
@@ -21,6 +43,7 @@ export default function SingleRecipe(props) {
           <Typography fontSize="large">Single Recipe</Typography>
         </Breadcrumbs>
         <h2>Single Recipe</h2>
+        <h1>{data.title}</h1>
       </main>
     </Wrapper>
   );
