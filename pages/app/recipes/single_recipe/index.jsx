168ca68a7fdd2f3d07523082c3c52b6a89ac7f56
Your Name <anthony@Anthonys-MacBook-Pro-2.local>
2024-07-21 11:43:12 -0400

    finished skeleton of single recipe page will refine another time

--- pages/app/recipes/single_recipe/index.jsx
+++ pages/app/recipes/single_recipe/index.jsx
@@ -1,4 +1,12 @@
-import { Breadcrumbs, Item, Grid, Typography } from "@mui/material";
+import {
+  Breadcrumbs,
+  Item,
+  Grid,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+} from "@mui/material";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { Wrapper } from "../../layout/wrapper";
 import styles from "../recipes.module.scss";
@@ -7,13 +15,15 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function SingleRecipe(props) {
-  const params = typeof window !== "undefined" ? window.location.search : Error;
+  const params =
+    typeof window !== "undefined"
+      ? window.location.search
+      : console.log("something went wrong!");
   const recipe = new URLSearchParams(params).getAll("id")[0];
   const [data, setData] = useState("Recipe");
   useEffect(() => {
     (async () => {
       const url = `/api/recipes/get_single_recipe?id=${recipe}`;
-      console.log(url);
       try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -30,11 +40,25 @@ export default function SingleRecipe(props) {
   }, []);
 
   function PrepsList(data) {
-      const steps = data.steps.map((step) => (
-        <li>{step}</li>
-      ));
-      return <ol>{steps}</ol>;
-    }
+    let steps = data.steps.map((step) => (
+      <ListItem divider="true">{step}</ListItem>
+    ));
+    return <List>{steps}</List>;
+  }
+
+  function DirectionsList(data) {
+    let steps = data.steps.map((step) => (
+      <ListItem divider="true">{step}</ListItem>
+    ));
+    return <List>{steps}</List>;
+  }
+
+  function IngredientsList(data) {
+    let ingredients = data.ingredients.map((ingredient) => (
+      <ListItem divider="true">{ingredient}</ListItem>
+    ));
+    return <List>{ingredients}</List>;
+  }
 
   return (
     <Wrapper className={props.className}>
@@ -52,18 +76,22 @@ export default function SingleRecipe(props) {
           <Typography fontSize="large">Single Recipe</Typography>
         </Breadcrumbs>
         <Link href="#">Update Recipe</Link>
-        <Grid container direction="column">
-          <Grid item xs={8}>
+        <Grid container direction="row">
+          <Grid item xs={7}>
             <Grid container direction="column" style={{ height: "100%" }}>
               <Grid item>
-                <h1 style={{ background: "pink" }}>{data.title}</h1>
+                <Typography variant="h1">
+                  <span style={{ borderBottom: "8px solid green" }}>
+                    {data.title}
+                  </span>
+                </Typography>
               </Grid>
               <Grid item>
                 <Grid container>
-                  <Grid item>
-                    <div style={{ background: "teal" }}>
-                      ratings and favorited
-                    </div>
+                  <Grid item style={{ paddingTop: "12px" }}>
+                    <Typography variant="h5">
+                      ***** Ratings and Favorited
+                    </Typography>
                     {/* reviews stars go here */}
                   </Grid>
                   <Grid item>{/* favorite icon goes here */}</Grid>
@@ -72,66 +100,84 @@ export default function SingleRecipe(props) {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={5}>
             <Grid
               container
               direction="row"
               alignItems="flex-start"
-              style={{ backgroundColor: "yellow", height: "100%" }}
+              style={{ height: "100%" }}
             >
               <Grid item xs={12}>
                 <Grid container>
-                  <Grid item xs={6}>
-                    <div style={{ "text-align": "right" }}>Prep Time:</div>
+                  <Grid item xs={4}>
+                    <Typography variant="h5" align="right">
+                      Prep Time:
+                    </Typography>
                   </Grid>
-                  <Grid item xs={6}>
-                    <div
-                      style={{ "text-align": "right", background: "orange" }}
-                    >
-                      {data.prep_time}
-                    </div>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container>
-                  <Grid item xs={6}>
-                    <div style={{ "text-align": "right" }}>Ingredients:</div>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <div style={{ "text-align": "right", background: "lime" }}>
-                      {data.ingredients}
-                    </div>
+                  <Grid item xs={8}>
+                    <Typography variant="h5" align="left">
+                      &nbsp;{data.prep_time}
+                    </Typography>
                   </Grid>
                 </Grid>
               </Grid>
               <Grid item xs={12}>
                 <Grid container>
-                  <Grid item xs={6}>
-                    <div style={{ background: "gray", "text-align": "right" }}>
-                      Cook Time:
-                    </div>
+                  <Grid item xs={4}>
+                    <Typography variant="h5" align="right">
+                      Ingredients:
+                    </Typography>
                   </Grid>
-                  <Grid item xs={6}>
-                    <div style={{ "text-align": "right", background: "green" }}>
-                      {data.cook_time}
-                    </div>
+                  <Grid item xs={8}>
+                    <Typography variant="h5" align="left">
+                      &nbsp;{data.ingredients}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <Typography variant="h5" align="right">
+                      &nbsp;Cook Time:
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Typography variant="h5" align="left">
+                      &nbsp;{data.cook_time}
+                    </Typography>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item>
+          <Grid item xs={12}>
             <Grid container>
               <Grid item xs={6} style={{ minHeight: "250px" }}>
                 <Grid container direction="column">
                   <Grid item>
                     {/* Description goes here */}
-                    <div>Descriptions</div>
+                    <Typography variant="h2">
+                      <span style={{ borderBottom: "6px solid green" }}>
+                        Description
+                      </span>
+                    </Typography>
+                    <Typography variant="body1" style={{ paddingTop: "12px" }}>
+                      {data.description}
+                    </Typography>
                   </Grid>
                   <Grid item>
                     {/* Ingredients go here */}
-                    <div>Ingredients</div>
+                    <Typography variant="h2">
+                      <span style={{ borderBottom: "6px solid green" }}>
+                        Ingredients
+                      </span>
+                    </Typography>
+                    {Array.isArray(data.ingredient) ? (
+                      <IngredientsList ingredients={data.ingredient} />
+                    ) : (
+                      <p>Loading ingredients</p>
+                    )}
                   </Grid>
                 </Grid>
               </Grid>
@@ -142,21 +188,79 @@ export default function SingleRecipe(props) {
                     width={250}
                     height={250}
                     alt="Picture of the author"
+                    style={{ border: "4px solid #000" }}
                   />
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item>
+          <Grid item xs={12}>
             <Grid container direction="row">
               <Grid item xs={6}>
                 {/* Cooking prep goes here */}
-                <div>Prep</div>
-                {Array.isArray(data.prep) ? <PrepsList steps={data.prep} />: <p>Loading prep steps</p>}
+                <Typography variant="h2">
+                  <span style={{ borderBottom: "6px solid green" }}>Prep</span>
+                </Typography>
+                {Array.isArray(data.prep) ? (
+                  <PrepsList steps={data.prep} />
+                ) : (
+                  <p>Loading prep steps</p>
+                )}
               </Grid>
               <Grid item xs={6}>
                 {/* Cooking instructions go here */}
-                <div>Directions</div>
+                <Typography variant="h2">
+                  <span style={{ borderBottom: "6px solid green" }}>
+                    Directions
+                  </span>
+                </Typography>
+                {Array.isArray(data.directions) ? (
+                  <DirectionsList steps={data.directions} />
+                ) : (
+                  <p>Loading cooking directions</p>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h2">
+              <span style={{ borderBottom: "6px solid green" }}>
+                Similar Recipes
+              </span>
+            </Typography>
+            <Grid container direction="row">
+              <Grid item xs={3}>
+                1
+              </Grid>
+              <Grid item xs={3}>
+                2
+              </Grid>
+              <Grid item xs={3}>
+                3
+              </Grid>
+              <Grid item xs={3}>
+                4
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h2">
+              <span style={{ borderBottom: "6px solid green" }}>
+                Interesting Blog Posts
+              </span>
+            </Typography>
+            <Grid container direction="row">
+              <Grid item xs={3}>
+                1
+              </Grid>
+              <Grid item xs={3}>
+                2
+              </Grid>
+              <Grid item xs={3}>
+                3
+              </Grid>
+              <Grid item xs={3}>
+                4
               </Grid>
             </Grid>
           </Grid>
